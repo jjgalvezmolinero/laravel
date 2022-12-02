8790a02259d07a4f7aa72d834cfd8fef75945ebe
Juan José Gálvez Molinero <juanjosegalvezmolinero@hotmail.com>
2022-12-02 23:43:27 +0100

    Se ha añadido el plugin de vite

--- devstream/resources/views/layouts/app.blade.php
+++ devstream/resources/views/layouts/app.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Navegacion</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navegacion</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-  <nav>
-    <a href="/devstream/public/">Principal</a>
-    <a href="/devstream/public/hola">Hola</a>
-    <a href="/devstream/public/tienda">Tienda</a>
-  </nav>
-  <h1>@yield("titulo")</h1>
-  <hr>
-  <footer>@yield("footer")</footer>
+    <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+    <hr>
+    @yield('contenido')
 </body>
+
 </html>
